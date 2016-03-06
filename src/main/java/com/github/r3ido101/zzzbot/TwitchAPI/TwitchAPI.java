4151a4dae8af1353c7c6361cc0c2b45e89d17958
R3ido101 <oliver_reid_@hotmail.co.uk>
2016-03-06 04:24:15 +0000

    Added some files for TwitchAPI ready for impllementing the Twitch api

--- src/main/java/com/github/r3ido101/zzzbot/TwitchAPI/TwitchAPI.java
+++ src/main/java/com/github/r3ido101/zzzbot/TwitchAPI/TwitchAPI.java
@@ -1,9 +1,8 @@
 package com.github.r3ido101.zzzbot.TwitchAPI;
 
-/**
- * Created by Oliver on 05/03/2016.
- */
-public class TwitchAPI {
+
+public class TwitchAPI
+{
 
     //TODO: This is where the API for Twitch will be place when i can be bothered to get round to it :)
 
